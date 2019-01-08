76700dc885da2f62091661b2e38b0abbba6bae31
IgnacioRubio <ignaciorubio3@gmail.com>
2019-01-08 11:32:27 +0100

    Refactored port-table service

--- src/app/services/port-table.service.ts
+++ src/app/services/port-table.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Port } from '../classes/port';
 
@@ -10,33 +10,34 @@ import { Port } from '../classes/port';
 })
 export class PortTableService {
 
-  portTableUrl:string = 'http://localhost:1880/ports-table';
+  portTableUrl: string = 'http://localhost:1880/ports-table';
 
   constructor(
     private http: HttpClient) { }
 
-
-  getPortTable(): Observable<any>{
-    return this.http.get<any>(this.portTableUrl).pipe(
+  // returns a Port array with the content from the server
+  getPortTable(): Observable<Port[]> {
+    return this.http.get<Port[]>(this.portTableUrl).pipe(
       map(record => {
-        return this.process(record['record']);
+        return this.processPortTable(record['record']);
       })
     );
   }
 
-  process(data:any)  
-    {
-        let dataTransform: Port[] = [];
-        
-        for (let item of data) {
-          let tempPort: Port = {
-            name : item['name'] || '<no name>',
-            numberPort : item['number'] || '<no number>',
-            transportProtocol : item['protocol'] || '<no protocol>',
-            description : item['description'] || '<no description>'
-          };
-          dataTransform.push(tempPort);
-        }
-        return dataTransform;
+  // process data from server
+  processPortTable(data: any): Port[] {
+    let dataTransform: Port[] = [];
+    
+    for (let item of data) {
+      let tempPort: Port = {
+        name : item['name'] || '<no name>',
+        numberPort : item['number'] || '<no number>',
+        transportProtocol : item['protocol'] || '<no protocol>',
+        description : item['description'] || '<no description>'
+      };
+
+      dataTransform.push(tempPort);
     }
+    return dataTransform;
+  }
 }
